acdb54bc619746142279d3ca463bd9b805daf1a6
Giovanni <giova.roncoroni@gmail.com>
2021-10-26 15:57:08 -0300

    Added categories

--- objectJS.js
+++ objectJS.js
@@ -1,13 +1,34 @@
 const quesos = [
-  { nombre: "Porsalut", precioXKilo: "$10", stock: 5 },
-  { nombre: "Sardo", precioXKilo: "$20", stock: 5 },
-  { nombre: "Cremoso", precioXKilo: "$30", stock: 5 },
-  { nombre: "Parmesano", precioXKilo: "$40", stock: 5 },
+  { nombre: "Porsalut", precioXKilo: 10, stock: 5 },
+  { nombre: "Sardo", precioXKilo: 20, stock: 5 },
+  { nombre: "Cremoso", precioXKilo: 30, stock: 5 },
+  { nombre: "Parmesano", precioXKilo: 40, stock: 5 },
 ];
-const lacteos = [
-  { nombre: "Leche", precio: "$10", stock: 5 },
-  { nombre: "Yogurt", precio: "$20", stock: 5 },
-  { nombre: "Crema", precio: "$30", stock: 5 },
-  { quesos },
+const leches = [
+  { nombre: "Entera", precio: 10, stock: 5 },
+  { nombre: "Descremada", precio: 20, stock: 5 },
+  { nombre: "Condensada", precio: 30, stock: 5 },
+  { nombre: "Deslactosada", precio: 40, stock: 5 },
+  { nombre: "En polvo", precio: 50, stock: 5 },
+  { nombre: "Extracalcio", precio: 40, stock: 5 },
 ];
-//qué conviene usar?: module.exports || object.json 
+const mantecas = [
+  { nombre: "Con sal", precio: 10, stock: 5 },
+  { nombre: "Sin sal", precio: 20, stock: 5 },
+];
+
+const yogurt = [
+  { nombre: "Firme", precio: 10, stock: 5 },
+  { nombre: "Bebible", precio: 20, stock: 5 },
+  { nombre: "Griego", precio: 30, stock: 5 },
+  { nombre: "Saborizado", precio: 40, stock: 5 },
+  { nombre: "Batido", precio: 50, stock: 5 },
+];
+const cremas = [
+  { nombre: "Liviana", precio: 10, stock: 5 },
+  { nombre: "Chantilly", precio: 20, stock: 5 },
+  { nombre: "Para batir", precio: 30, stock5 },
+];
+const lacteos = [{ leches }, { mantecas }, { yogurt }, { cremas }, { quesos }];
+//qué conviene usar?: module.exports || object.json
+
