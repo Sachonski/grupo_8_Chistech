c8b082009981c480e23bc946569f8e989456f21d
Giovanni <giova.roncoroni@gmail.com>
2021-10-26 16:17:04 -0300

    Added Dulce

--- objectJS.js
+++ objectJS.js
@@ -3,6 +3,8 @@ const quesos = [
   { nombre: "Sardo", precioXKilo: 20, stock: 5 },
   { nombre: "Cremoso", precioXKilo: 30, stock: 5 },
   { nombre: "Parmesano", precioXKilo: 40, stock: 5 },
+  { nombre: "Finlandia", precioXKilo: 50, stock: 5 },
+  { nombre: "Untable", precioXKilo: 25, stock: 5 },
 ];
 const leches = [
   { nombre: "Entera", precio: 10, stock: 5 },
@@ -15,6 +17,7 @@ const leches = [
 const mantecas = [
   { nombre: "Con sal", precio: 10, stock: 5 },
   { nombre: "Sin sal", precio: 20, stock: 5 },
+  { nombre: "Light", precio: 30, stock: 5 },
 ];
 
 const yogurt = [
@@ -29,6 +32,18 @@ const cremas = [
   { nombre: "Chantilly", precio: 20, stock: 5 },
   { nombre: "Para batir", precio: 30, stock5 },
 ];
-const lacteos = [{ leches }, { mantecas }, { yogurt }, { cremas }, { quesos }];
+const dulceDeLeches = [
+  { nombre: "Colonial", precio: 10, stock: 5 },
+  { nombre: "Tradicional", precio: 20, stock: 5 },
+  { nombre: "Clásico", precio: 30, stock: 5 },
+  { nomnbre: "Repostero", precio: 40, stock: 5 },
+];
+const lacteos = [
+  { leches },
+  { mantecas },
+  { yogurt },
+  { cremas },
+  { quesos },
+  { dulceDeLeches },
+];
 //qué conviene usar?: module.exports || object.json
-
