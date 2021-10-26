87ca5af21d822aded0411d0106285528d16d12bd
Giovanni <giova.roncoroni@gmail.com>
2021-10-26 04:14:55 -0300

    Added object

---
+++ objectJS.js
@@ -0,0 +1,13 @@
+const quesos = [
+  { nombre: "Porsalut", precioXKilo: "$10", stock: 5 },
+  { nombre: "Sardo", precioXKilo: "$20", stock: 5 },
+  { nombre: "Cremoso", precioXKilo: "$30", stock: 5 },
+  { nombre: "Parmesano", precioXKilo: "$40", stock: 5 },
+];
+const lacteos = [
+  { nombre: "Leche", precio: "$10", stock: 5 },
+  { nombre: "Yogurt", precio: "$20", stock: 5 },
+  { nombre: "Crema", precio: "$30", stock: 5 },
+  { quesos },
+];
+//qué conviene usar?: module.exports || object.json 
