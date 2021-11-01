8d384ee739bcee471d129c3b21e2f38137daef41
Giovanni <giova.roncoroni@gmail.com>
2021-11-01 18:34:11 -0300

    Agregados

--- scripts.js
+++ scripts.js
@@ -1,10 +1,24 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const port = 3030 
+const port = 3030;
 app.use(express.static("public"));
 
-
-app.get('/carrito', (req, res) => {
-    res.sendFile(path.join(__dirname + '/views/carrito.html'))
-  })
+app.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/login.html"));
+});
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/register.html"));
+});
+app.get("/sobreNosotros", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/sobreNosotros.html"));
+});
+app.get("/carrito", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/carrito.html"));
+});
+app.get("/productos", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/productos.html"));
+});
