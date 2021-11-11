47ee170510fd665ebdaa6323bc19ce7b40d84b87
Nicolas Cano <nicolascano40@hotmail.com>
2021-11-11 14:29:40 -0300

    Hecho el esqueleto del index.html

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const path = require("path");
+const port = 3030
 const app = express();
-const port = 3030;
-app.use(express.static("public"));
+
+app.use(express.static(path.resolve(__dirname, "./public")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/views/index.html"));
