fc4beb183ccee5e240f8ac8051a4fcb652caad0a
Giovanni <giova.roncoroni@gmail.com>
2021-11-13 14:30:49 -0300

    header navbar

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const port = 3030
 const app = express();
-const port = 3030;
+
 
 app.use(express.static(path.resolve(__dirname, "./public")));
 
