cfe909874bf5529f72daac06cb3dada2b2c99dbc
Alejandro Telatnik <alejandrojtelatnik@gmail.com>
2021-11-13 14:30:07 -0300

    Updated retro to readme

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ app.get("/productos", (req, res) => {
   res.sendFile(path.join(__dirname + "/views/productos.html"));
 });
 app.listen(port, () => {
-  console.log("Server running on port " + port);
+  console.log("Server running on port " + "http://localhost:"+port);
 });
