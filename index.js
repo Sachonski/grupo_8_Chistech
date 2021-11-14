fea0b867faa26b6346ac69188e28b16ab36dab75
Alejandro Telatnik <alejandrojtelatnik@gmail.com>
2021-11-13 21:04:20 -0300

    Arreglo home

--- index.js
+++ index.js
@@ -1,38 +1,29 @@
-const express = require ('express');
+const express = require("express");
+const path = require("path");
+const port = 3030
 const app = express();
-const port = 3030;
-const path = require('path');
 
 
-app.listen(port, () => { console.log("Server running on http://localhost/" + port)});
-
+app.use(express.static(path.resolve(__dirname, "./public")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/views/home.html"));
-
-app.use(express.static('public'));
-
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, 'view/home.html'))
-
 });
-
 app.get("/login", (req, res) => {
-    res.sendFile(path.join(__dirname + "/views/login.html"));
-  });
-  app.get("/register", (req, res) => {
-    res.sendFile(path.join(__dirname + "/views/register.html"));
-  });
-  app.get("/sobreNosotros", (req, res) => {
-    res.sendFile(path.join(__dirname + "/views/sobreNosotros.html"));
-  });
-  app.get("/carrito", (req, res) => {
-    res.sendFile(path.join(__dirname + "/views/carrito.html"));
-  });
-  app.get("/productos", (req, res) => {
-    res.sendFile(path.join(__dirname + "/views/productos.html"));
-  });
-  app.listen(port, () => {
-    console.log("Server running on port " + "http://localhost:"+port);
-  });
+  res.sendFile(path.join(__dirname + "/views/login.html"));
+});
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/register.html"));
+});
+app.get("/sobreNosotros", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/sobreNosotros.html"));
+});
+app.get("/carrito", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/carrito.html"));
+});
+app.get("/productos", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/productos.html"));
+});
+app.listen(port, () => {
+  console.log("Server running on port " + "http://localhost:"+port);
 });
