22fd1253181df7ec1f7877b2ed7f359a94d09040
John DoeDiana <dadrover75@hotmail.com>
2021-12-05 19:02:10 -0300

    ejs y nuevas carpetas

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, "./public")));
 
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/views/home.html"));
 });
