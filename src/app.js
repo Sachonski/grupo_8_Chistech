78dce0da6447f6c57cd017a7f7b6ac5bc1ce3dea
John DoeDiana <dadrover75@hotmail.com>
2021-12-05 19:58:08 -0300

    sistema de controladores y ruteo andando

---
+++ src/app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const mainRoutes = require("./routes/mainRoute");
+
+app.use(express.static("./public"));
+
+app.set("view engine", "ejs");
+
+app.use('/', mainRoutes);
+
+app.listen(process.env.PORT || 3030, () => console.log("Server running on port " + "http://localhost:" + 3030));
+
+/*app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/home.ejs"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/login.ejs"));
+});
+app.post("/login", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/login.ejs"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/register.ejs"));
+});
+app.post("/register", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/register.ejs"));
+});
+
+app.get("/sobreNosotros", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/sobreNosotros.ejs"));
+});
+
+app.get("/carrito", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/carrito.ejs"));
+});
+
+app.get("/productos", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/productos.ejs"));
+});
+
+app.get("/detalle-producto", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/detalle-producto.ejs"));
+});*/
+
---
+++ src/controllers/mainController.js
@@ -0,0 +1,26 @@
+const controller = {
+    // Initialize the controller
+    home: (req, res) => {
+        return res.render('home');
+    },
+    productos: (req, res) => {
+        return res.render('productos');
+    },
+    detalleProducto: (req, res) => {
+        return res.render('detalle-producto');
+    },
+    carrito: (req, res) => {
+        return res.render('carrito');
+    },
+    login: (req, res) => {
+        return res.render('login');
+    },
+    register: (req, res) => {
+        return res.render('register');
+    },
+    sobreNosotros: (req, res) => {
+        return res.render('sobreNosotros');
+    }
+}
+
+module.exports = controller;
