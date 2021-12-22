7198e1a2cc2a2cb3556eb6811cafe89a881b8e34
John DoeDiana <dadrover75@hotmail.com>
2021-12-21 21:42:27 -0300

    falta que funcione bien cargar el detalle producto

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -3,9 +3,6 @@ const path = require('path');
 
 const productsFilePath = path.join(__dirname, '../../data/products.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-const toThousand = n => n.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-
-
 
 
 const controller = {
@@ -23,8 +20,8 @@ const controller = {
     },
     detalleProducto: (req, res) => {
         const id = req.params.id;
-        const product = products.find(product => product.id == id);
-        return res.render('detalle-producto',  product[0] );
+        const product = products.filter(product => product.id == id);
+        return res.render('detalle-producto',  product[0]);
     },
     carrito: (req, res) => {
         return res.render('carrito');
@@ -47,7 +44,7 @@ const controller = {
         const newProduct = {}
         newProduct.id = products[products.length - 1].id + 1;
         newProduct.name = name;
-        newProduct.price = toThousand(parseInt(price));
+        newProduct.price = parseInt(price);
         newProduct.discount = parseInt(discount);
         newProduct.category = category;
         newProduct.description = description;
