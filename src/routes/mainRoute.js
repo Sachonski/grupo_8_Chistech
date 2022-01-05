a7f6e4f7b6c887dd888eb3b7b85a33d79bb001af
Nicolas Cano <nicolascano40@hotmail.com>
2022-01-05 15:59:12 -0300

    arreglo

--- src/routes/mainRoute.js
+++ src/routes/mainRoute.js
@@ -5,19 +5,17 @@ const multer = require("multer");
 const date = Date.now();
 const path = require("path");
 
-
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     // cb(null, "../public/img");
-    cb(null, path.join(__dirname, '../../public/img'))
+    cb(null, path.join(__dirname, "../../public/img"));
   },
-   filename: function (req, file, cb) {
-     cb(null, date +"_"+file.originalname);
- }
+  filename: function (req, file, cb) {
+    cb(null, date + "_" + file.originalname);
+  },
 });
 
-const uploadFile = multer({ storage});
-
+const uploadFile = multer({ storage });
 
 router.get("/", mainController.home);
 router.get("/productos", mainController.productos);
@@ -28,12 +26,19 @@ router.get("/login", mainController.login);
 router.get("/register", mainController.register);
 router.get("/sobreNosotros", mainController.sobreNosotros);
 
-
 router.get("/productoCreacion", mainController.productoCreacion);
-router.post('/productoCreacion' , uploadFile.single('image'), mainController.productoGuardar);
+router.post(
+  "/productoCreacion",
+  uploadFile.single("image"),
+  mainController.productoGuardar
+);
 router.get("/productoEdicion/:id/", mainController.productEdit);
-router.put('/productoEdicion/:id/' , uploadFile.single('image'), mainController.productUpdate);
+router.put(
+  "/productoEdicion/:id/",
+  uploadFile.single("image"),
+  mainController.productUpdate
+);
 
-router.delete('/productoDelete/:id', mainController.productDestroy);
+router.delete("/productoDelete/:id", mainController.productDestroy);
 
 module.exports = router;
