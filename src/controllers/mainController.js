b01db4b4c460d22b64194b1375a16f9a44097f1f
John DoeDiana <dadrover75@hotmail.com>
2022-01-16 19:43:52 -0300

    ultimo del dia domingo

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -25,12 +25,6 @@ const controller = {
     carrito: (req, res) => {
         return res.render('carrito');
     },
-    login: (req, res) => {
-        return res.render('login');
-    },
-    register: (req, res) => {
-        return res.render('register');
-    },
     sobreNosotros: (req, res) => {
         return res.render('sobreNosotros');
     },
