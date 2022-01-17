f735b984b92e4bc032f559562876e3d7538a3ecd
John DoeDiana <dadrover75@hotmail.com>
2022-01-17 16:31:59 -0300

    mensaje de error de session

--- src/middlewares/session.js
+++ src/middlewares/session.js
@@ -1,6 +1,6 @@
 const userSession = (req, res, next) => {
     if(!req.session.user){
-        res.send(404);
+        res.send('Esta pagina es solo para usuarios. Registrate o inicia sesion: <a href="/users/register">Registrarse</a> o <a href="/users/login">Iniciar sesion</a>');
     } else {
        next();
     }
