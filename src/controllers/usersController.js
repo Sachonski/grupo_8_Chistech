b94d9e4935a4a6e7706375fbbc0dc7259a6247f6
John DoeDiana <dadrover75@hotmail.com>
2022-01-17 20:57:09 -0300

    empezando con modelo User

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 const path = require('path');
+const bcryptjs = require('bcryptjs');
+const { validationResult } = require('express-validator');
 
 const usersFilePath = path.join(__dirname, '../../data/users.json');
 const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
-const { validationResult } = require('express-validator');
+const User = require('../models/User');
 
 const controller2 = {
     // Initialize the controller
